8967a1347cdb9618fbd5f7cb3994dfb17e1ece98
Igor Felipe <igor_play_22@hotmail.com>
2018-04-04 10:34:46 -0300

    :construction: - Montando estrutura do projeto

---
+++ src/components/Button.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="starwars__button">
+      {{title}}
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Button',
+  props: [
+      'title',
+  ],
+}
+</script>
+
+<style lang="scss" scoped>
+.starwars__button{
+    width: 100%;
+    height: 50px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background-color: #0984e3;
+    color: white;
+    font-weight: bold;
+    text-transform: uppercase;
+    font-size: 18px;
+    font-family: Arial, Helvetica, sans-serif;
+    letter-spacing: 1px;
+}
+</style>
---
+++ src/pages/Home.vue
@@ -0,0 +1,18 @@
+<template>
+  <starwarsbtn title="Start"></starwarsbtn>
+</template>
+
+<script>
+import Button from '@/components/Button';
+
+export default {
+  name: 'Home',
+  components:{
+      'starwarsbtn': Button,
+  }
+}
+</script>
+
+<style lang="scss">
+
+</style>
